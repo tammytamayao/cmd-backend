f6b66e4a49011ccce79fa5be7ed1eccc1d2bce48
tammytamayao <tammytamayao@gmail.com>
2025-11-02 13:46:20 +0800

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :subscribers, only: [:index, :show]
+      resources :sessions, only: [:create, :destroy]
+    end
+  end
+
 end
