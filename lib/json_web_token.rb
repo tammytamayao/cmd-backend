549b54924e7eb89fb4ea45dcec04b3c79a7eacea
tammytamayao <tammytamayao@gmail.com>
2025-11-02 13:48:33 +0800

    feat: json web token

---
+++ lib/json_web_token.rb
@@ -0,0 +1,15 @@
+class JsonWebToken
+  SECRET = Rails.application.credentials.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET, "HS256")
+  end
+
+  def self.decode(token)
+    body, = JWT.decode(token, SECRET, true, { algorithm: "HS256" })
+    HashWithIndifferentAccess.new(body)
+  rescue JWT::DecodeError, JWT::ExpiredSignature
+    nil
+  end
+end
