d6d9455ea85b6f047451aadbf5a567e34028ab2c
Frostzeichen <cruztjomar@gmail.com>
2025-12-21 15:15:34 +0800

    style: whitespace in brackets

--- app/controllers/api/admin/payments_controller.rb
+++ app/controllers/api/admin/payments_controller.rb
@@ -32,7 +32,7 @@ class Api::Admin::PaymentsController < ApplicationController
 
     # ---- Pagination ----
     page     = (params[:page] || 1).to_i
-    per_page = [(params[:per_page] || 20).to_i, 100].min
+    per_page = [ (params[:per_page] || 20).to_i, 100 ].min
     total    = payments.count
     payments = payments.offset((page - 1) * per_page).limit(per_page)
 
