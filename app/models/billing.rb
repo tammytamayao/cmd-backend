df408e58042ab6cee7817f4c719a3a9bd33dd7d3
tammytamayao <tammytamayao@gmail.com>
2025-12-23 14:11:30 +0800

    feat: add validation for existing billing period

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,4 +1,11 @@
 class Billing < ApplicationRecord
-    belongs_to :subscriber
-    has_many :payments, dependent: :destroy
+  belongs_to :subscriber
+  has_many :payments, dependent: :destroy
+
+  validates :start_date, :end_date, :due_date, :amount, :status, presence: true
+
+  validates :subscriber_id, uniqueness: {
+    scope: [:start_date, :end_date],
+    message: "already has a billing for that period"
+  }
 end
